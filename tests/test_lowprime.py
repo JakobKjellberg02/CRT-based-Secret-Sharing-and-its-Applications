9dcf7e08b21d6f5306ec9d13d0814f96d7c40d3a
Jakob Zornig Kjellberg <jakob.zornigkjellberg@gmail.com>
2025-03-11 15:44:42 +0100

    Project structure and start of unit testing

---
+++ tests/test_lowprime.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath('src'))
+import share_distribution as sd
+import share_reconstruction as sr
+
+class TestWithALowPrimeAndLowThreshold(unittest.TestCase):
+
+    def test_low_prime(self):
+        d_0 = 3        
+        p = 283          
+        threshold = (7,3)    
+        n,t = threshold
+        m_0, moduli = sd.scheme_setup(p, d_0, n, t)
+        s, alpha = sd.generate_secret(p, d_0, moduli, t)
+        f, created_shares = sd.shares(s, alpha, m_0, moduli)
+        reconstruction_index = [0,1,2,3]
+        reconstruct_shares = [created_shares[i] for i in reconstruction_index]
+        reconstruct_moduli = [moduli[i] for i in reconstruction_index]
+
+        reconstructed_the_secret = sr.reconstruct_secret(reconstruct_shares, reconstruct_moduli
+                                                    , m_0, p)
+        self.assertEqual(reconstructed_the_secret, s)
+
+if __name__ == '__main__':
+    unittest.main()
+
+        
