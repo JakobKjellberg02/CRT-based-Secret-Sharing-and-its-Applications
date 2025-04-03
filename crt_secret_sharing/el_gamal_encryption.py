c8bc938bb9dcf478acf9eab6222945615ec6ab11
Jakob Zornig Kjellberg <jakob.zornigkjellberg@gmail.com>
2025-04-03 11:44:46 +0200

    Need major refactoring

--- crt_secret_sharing/el_gamal_encryption.py
+++ crt_secret_sharing/el_gamal_encryption.py
@@ -4,7 +4,7 @@ import secrets
 import sympy
 from crt_secret_sharing.util_crt import modinv
 from crt_secret_sharing.weighted_crt_share import WRSS_setup
-from Crypto.Util.number import getPrime, GCD
+from Crypto.Util.number import getPrime
 
 def universal_hashing(x):
     digest = hashes.Hash(hashes.SHA256())
@@ -20,28 +20,26 @@ def randomness_extractor(s, X, p):
     )
     return int.from_bytes(hkdfsha256.derive(str(X).encode()),'big') 
 
-def find_generator(p):
-    if not sympy.isprime(p):
-        raise ValueError("p must be prime to find generator")
-    phi = p - 1
-    factors = sympy.primefactors(phi)
+def find_generator(p, q):
+    if (p - 1) % q != 0:
+        raise ValueError("q must divide p-1")
 
     for g in range(2, p):
-        is_generator = True
-        for factor in factors:
-            if pow(g, phi // factor, p) == 1:
-                is_generator = False
-                break
-        if is_generator:
-            return g
-    raise RuntimeError(f"Could not find generator for prime {p}")
+        if pow(g, q, p) == 1:
+             if p == 2*q + 1: 
+                  if pow(g, 2, p) != 1:
+                       return g
+             else:
+                  return g
+
+    raise RuntimeError(f"Could not find generator of order {q} for prime {p}")
 
 def sample_group(p_lambda):
     while True:
         q = getPrime(p_lambda)
         p_0 = 2 * q + 1
-        if (GCD(q, p_0) == 1):
-            small_g = 2
+        if sympy.isprime(p_0):
+            small_g = find_generator(p_0, q)
             return p_0, q, small_g
 
 def keygen(p_lambda):
@@ -54,7 +52,7 @@ def encrypt(m, h, small_g, p_0, q):
     r = secrets.randbelow(q - 1)
     c1 = pow(small_g, r, p_0)
     k = pow(h, r, p_0)
-    sd = secrets.randbits(32)
+    sd = secrets.randbits(16)
     k_random = randomness_extractor(sd, k, p_0)
     c2 = m ^ k_random
     h_k = universal_hashing(k)
@@ -71,20 +69,17 @@ def langrange_coeffs(index, participants, p_i):
     inv_P = modinv(P, mod)
     return P * inv_P
     
-def partial_decrypt(index, share, c1, p_0, participants, p_i):
+def partial_decrypt(index, share, c1, p_0, participants, p_i, q):
     P_S = 1
     for i in participants:
         P_S *= p_i[i]
     lambda_i = langrange_coeffs(index, participants, p_i)
     exp = (share * lambda_i) % P_S
-    group_order = p_0 - 1
-    if group_order <= 0:
-         raise ValueError("Invalid group order (p_0 <= 1)")
-    final_exp = exp % group_order
+    final_exp = exp % q
     mu_i = pow(c1, final_exp, p_0)
     return mu_i
 
-def reconstruct(partial_decryptions, c1, h_k, p_0, participants, p_i):
+def reconstruct(partial_decryptions, c1, h_k, p_0, participants, p_i, q):
     mu = 1
     for i in participants:
         mu = (mu * partial_decryptions[i]) % p_0
@@ -95,7 +90,7 @@ def reconstruct(partial_decryptions, c1, h_k, p_0, participants, p_i):
     max_overflow = len(participants)
     print(f"Reconstructing: mu={mu}, c1={c1}, P_S={P}, p_0={p_0}")
     for j in range(max_overflow + 1):
-        exp_inv = (-j * P) % p_0
+        exp_inv = (-j * P) % q
         print("test")
 
         inv_factor = pow(c1, exp_inv, p_0) 
@@ -119,23 +114,23 @@ if __name__ == "__main__":
     weights = [16,17,18,19,20]
     p_lambda = 32
 
-    small_g, q, p_0, small_s, h = keygen(p_lambda)
+    p_0, q, small_g, small_s, h = keygen(p_lambda)
 
-    big_s, shares, p_i = WRSS_setup(p_0, small_s, weights, T, t, p_lambda)
+    big_s, shares, p_i = WRSS_setup(q, small_s, weights, T, t, p_lambda)
 
-    participants = {1,2,3}
+    participants = {3,4}
     session_weight = sum(weights[i] for i in participants)
 
-    plaintext = 420420420
+    plaintext = 420420
     ciphertext, random_r = encrypt(plaintext, h, small_g, p_0, q)
     c2, seed, c1, h_k = ciphertext
 
     partial_decryptions = {}
     for i in participants:
-        mu_i = partial_decrypt(i, shares[i], c1, p_0, participants, p_i)
+        mu_i = partial_decrypt(i, shares[i], c1, p_0, participants, p_i, q)
         partial_decryptions[i] = mu_i
 
-    k_constructed = reconstruct(partial_decryptions, c1, h_k, p_0, participants, p_i)
+    k_constructed = reconstruct(partial_decryptions, c1, h_k, p_0, participants, p_i, q)
     decrypted_message = decrypt(c2, k_constructed, seed, p_0)
     print(decrypted_message)
     
